0b2775586305e84c07da0b98c12a81b867cd3f22
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2025-02-01 12:55:11 +0100

    pridani postavy
    nutno udelat ovladani

--- muj_projekt_skakacka.py
+++ muj_projekt_skakacka.py
@@ -1,17 +1,17 @@
 import sys
 import pygame 
 
-# Inicializace Pygame
+
 pygame.init() 
 
-# Nastavení rozlišení
+
 rozliseni_vyska = 600
 rozliseni_sirka = 800
 
-# Vytvoření okna
+
 screen = pygame.display.set_mode((rozliseni_sirka, rozliseni_vyska))
 
-# Pokus o načtení obrázku
+
 try:
     background_image = pygame.image.load('backgroundColorForest.png')
     background_image = pygame.transform.scale(background_image, (rozliseni_sirka, rozliseni_vyska))
@@ -20,15 +20,31 @@ except pygame.error as e:
     pygame.quit()
     sys.exit()
 
-# Hlavní herní cyklus
+
+# Načtení obrázku postavy PŘED hlavní smyčkou
+try:
+    postava = pygame.image.load('ufo-removebg-preview.png').convert_alpha()
+except pygame.error as e:
+    print(f"Chyba při načítání obrázku postavy: {e}")
+    pygame.quit()
+    sys.exit()
+
+
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT: 
             pygame.quit()
             sys.exit()   
 
-    # Vykreslení pozadí
     screen.blit(background_image, (0, 0))
 
-    # Aktualizace obrazovky
+    
+    screen.blit(postava, (100, 90))  # Souřadnice X=100, Y=100
+
+   
+    
+    
+    
+
+    
     pygame.display.update()
