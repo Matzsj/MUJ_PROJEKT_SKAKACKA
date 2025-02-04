fe1b94df80956d0ad1a3f6cf0087d6bccc1c8bb0
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2025-02-04 15:52:09 +0100

    POKUS ZMENY POHYBU
    NENI TO HOTOVE

--- muj_projekt_skakacka.py
+++ muj_projekt_skakacka.py
@@ -18,17 +18,35 @@ pozice_y_hrace = 90
 skace = False
 zem = 90  # Výška země
 
+
+
+
+posun_sveta = 0
+
+
+prekazky = [
+    (300, 400, 50, 50), 
+    (800, 350, 50, 100),
+    (1200, 450, 60, 60),
+    (1600, 400, 70, 50)
+]
+
+
+RED = (255, 0, 0)
+
+
+
 # Vytvoření okna
 screen = pygame.display.set_mode((rozliseni_sirka, rozliseni_vyska))
 
 # Načtení obrázku pozadí
-try:
-    background_image = pygame.image.load('backgroundColorForest.png')
-    background_image = pygame.transform.scale(background_image, (rozliseni_sirka, rozliseni_vyska))
-except pygame.error as e:
-    print(f"Chyba při načítání obrázku: {e}")
-    pygame.quit()
-    sys.exit()
+#    try:
+ #       background_image = pygame.image.load('backgroundColorForest.png')
+  #      background_image = pygame.transform.scale(background_image, (rozliseni_sirka, rozliseni_vyska))
+   # except pygame.error as e:
+    #    print(f"Chyba při načítání obrázku: {e}")
+     #   pygame.quit()
+      #  sys.exit()
 
 # Načtení obrázku postavy
 try:
@@ -48,13 +66,17 @@ while True:
             if event.key == pygame.K_SPACE and not skace:  # Skok jen když nestojíme ve vzduchu
                 y_velocity = vyska_skoku
                 skace = True
-
+    
+    
+    
+    
     # Pohyb postavy
     stisknute_klavesy = pygame.key.get_pressed()
-    if stisknute_klavesy[pygame.K_d]:  
-        pozice_x_hrace += rychlost
-    if stisknute_klavesy[pygame.K_a]:  
-        pozice_x_hrace -= rychlost
+    if stisknute_klavesy[pygame.K_a]:
+        posun_sveta += rychlost  # Posune svět doprava
+
+    if stisknute_klavesy[pygame.K_d]:
+        posun_sveta -= rychlost
 
     # Aplikace gravitace
     pozice_y_hrace += y_velocity
@@ -67,7 +89,9 @@ while True:
         skace = False
 
     # Vykreslení
-    screen.blit(background_image, (0, 0))
+    for x, y, w, h in prekazky:
+        pygame.draw.rect(screen, RED, (x + posun_sveta, y, w, h))
+        
     screen.blit(postava, (pozice_x_hrace, pozice_y_hrace))
 
     pygame.display.update()
